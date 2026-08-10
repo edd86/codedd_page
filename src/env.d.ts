7d809a49aa8968555ba5365153ef4e66804c9782
Edward Cristobal Diaz Valda <edward.cristob@gmail.com>
2026-08-09 20:18:41 -0400

    chore(config): update project configuration and dependencies

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,8 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    [elemName: string]: any;
+  }
+}
